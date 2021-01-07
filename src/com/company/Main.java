ae5043f0e42e599b2ec9cab28f158cc4edae164d
kevarey reynolds <kevareynolds@gmail.com>
2021-01-07 14:26:28 -0600

    errors in java

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //error cannot find symbol string, String should be capitalized
+        //string name = "Bikola";
+        //System.out.println(name);
+        //error java.lang.String cannot be converted to byte
+        //byte num = (byte) "Kim";
+        //error java: ';' expected
+        // byte sumNum = (byte) 12
+        //error java: reached end of file while parsing, missing '}'
+//     if(true) {
+//         String x = "Hello";
+
+
+      //  error java: cannot find symbol,
+        //  symbol: variable i, location: class com.company, missing int for i
+//       for(i = 0; i < 10; i++){
+//        String x = "got it";}
+        System.out.print("All above are errors! Don't worry!");
+    }
+}
